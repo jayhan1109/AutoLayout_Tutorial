56902ee61970c2b9d966bcb1cbf45c6d4665babb
jayhan1109 <jungho1109@gmail.com>
2021-05-21 15:39:24 -0700

    Initial Basic Tutorial

---
+++ Basic/Basic_1_Lecture/Basic_1_Lecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic_1_Lecture
+//
+//  Created by Jeongho Han on 2021-05-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
